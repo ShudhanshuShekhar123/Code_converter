2cadbe23f847d51214e6c900ed8a7180ff26e2d2
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-09-14 22:04:37 +0530

    add

--- index.js
+++ index.js
@@ -10,77 +10,58 @@ const apiopen = new openai({
 });
 
 
-app.post("/convert", (req,res)=>{
-    const {prompt, code} = req.body
+app.post("/convert", (req, res) => {
+  const { prompt, code } = req.body
 
-    async function main() {
+  async function main() {
 
-        const completion = await apiopen.chat.completions.create({
-          messages: [{ role: 'user', content: `Just convert   for the  ${code}  into ${prompt} and dont explain the code `}],
-          model: 'gpt-3.5-turbo',
-        });
-      
-        console.log(completion.choices);
-        res.send(completion.choices[0].message.content)
-      }
-      
-      main();
+    const completion = await apiopen.chat.completions.create({
+      messages: [{ role: 'user', content: `Just convert   for the  ${code}  into ${prompt} and dont explain the code ` }],
+      model: 'gpt-3.5-turbo',
+    });
+
+    console.log(completion.choices);
+    res.send(completion.choices[0].message.content)
+  }
+
+  main();
 
 })
 
+app.post("/choice", async (req, res) => {
 
-
-app.post("/choice",async (req, res)=>{
-
-    const {code, prompt} = req.body
-    let newcode ;
-    if(prompt == "debug"){
-        newcode = `tell the errors in the ${code} and then provide the corrrect code but keep the xplaination to the point and short`
-    }else 
-    if(prompt == "check quality"){
-        newcode = `Please provide a  code quality assessment  in brief for the given code   ${code}. Consider the following parameters: 1. Code Consistency, 2.Code Performance,3.Code Documentation,4.Error Handling, 5.Code Testability, 6.Code Complexity, 7.Code Duplication, 8.Code Readability  and aslo provide evaluation score in % every parameter an at last give overall score.`
+  const { code, prompt } = req.body
+  let newcode;
+  if (prompt == "debug") {
+    newcode = `tell the errors in the ${code} and then provide the corrrect code but keep the xplaination to the point and short`
+  } else
+    if (prompt == "check quality") {
+      newcode = `Please provide a  code quality assessment  in brief for the given code   ${code}. Consider the following parameters: 1. Code Consistency, 2.Code Performance,3.Code Documentation,4.Error Handling, 5.Code Testability, 6.Code Complexity, 7.Code Duplication, 8.Code Readability  and aslo provide evaluation score in % every parameter an at last give overall score.`
     }
-      
 
-    //Using chatgpt trxt completion api 
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      messages: [{ "role": "user", "content": newcode }],
+      model: "gpt-3.5-turbo",
+      temperature: 0.5,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    });
 
-    // async function main() {
+    const codequality = response.data.choices[0].message.content;
+    console.log(codequality)
+    res.json({ codequality });
+  } catch (error) {
+    console.error('Error converting code:', error);
+    res.status(500).json({ error });
+  }
 
-    //     const completion = await apiopen.chat.completions.create({
-    //       messages: [{ role: 'user', content: newcode }],
-    //       model: 'gpt-3.5-turbo',
-    //     });
-      
-    //     console.log(completion.choices);
-    //     res.send(completion.choices[0].message.content)
-    //   }
-      
-    //   main();
-
-
-    try {
-        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-          messages:[{"role":"user","content":newcode}],
-          model:"gpt-3.5-turbo",
-          temperature: 0.5,
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-          },
-        });
-    
-        const codequality=response.data.choices[0].message.content;
-        console.log(codequality)
-        res.json({ codequality });
-      } catch (error) {
-        console.error('Error converting code:', error);
-        res.status(500).json({ error});
-      }
-  
 
 })
 
-app.listen(7000, ()=>{
-    console.log("listening at port 7000")
+app.listen(7000, () => {
+  console.log("listening at port 7000")
 })
