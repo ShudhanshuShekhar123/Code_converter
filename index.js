a0a7898ad08d075b482c808f3b652d5c238f96f7
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-09-14 22:14:03 +0530

    add

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const axios = require("axios")
 const app = express()
 const openai = require("openai")
+const cors = require('cors'); 
 require('dotenv').config();
+app.use(cors()); 
 app.use(express.json())
+
 
 const apiopen = new openai({
   apiKey: process.env.OPENAI_API_KEY
